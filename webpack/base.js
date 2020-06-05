e90fef44c1eefda3e093852631c96980e73aa844
Olina <olina.stath@nyu.edu>
2020-06-06 00:19:10 +0100

    fix host hopefully

--- webpack/base.js
+++ webpack/base.js
@@ -5,6 +5,10 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devServer: {
+    port: process.env.PORT || 3000,
+    host: 'https://bean-game-client.herokuapp.com'
+  },
   devtool: "eval-source-map",
   module: {
     rules: [
