5e64ccd3026d95138de2e404769aaeb4c59e5b11
Olina <olina.stath@nyu.edu>
2020-06-06 01:02:45 +0100

    fix host hopefully

--- webpack/base.js
+++ webpack/base.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: "development",
   devServer: {
     port: process.env.PORT || 3000,
-    host: process.env.HOST || 'localhost'
+    host: process.env.HOST || '0.0.0.0'
   },
   devtool: "eval-source-map",
   module: {
